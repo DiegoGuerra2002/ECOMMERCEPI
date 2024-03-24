2eb1cea982ecb9999b7e3e2e798a6476765c72c4
DiegoGuerra2002 <99612849+DiegoGuerra2002@users.noreply.github.com>
2024-03-23 22:18:48 -0600

    Add files via upload

---
+++ storage/framework/views/092771252f7f9af0b7d6014051884d54.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1> Este es el inicio de un sistema enorme </h1>
+</body>
+</html><?php /**PATH C:\laragon\www\ECommercePI\resources\views/welcome.blade.php ENDPATH**/ ?>
